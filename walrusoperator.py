9b475cb608288bc241334ce60dd285fb5714963a
Zamishailo Artem <72965392+ZamArtem@users.noreply.github.com>
2022-12-31 20:01:29 +0100

    Add files via upload

---
+++ walrusoperator.py
@@ -0,0 +1,17 @@
+
+#happy = True
+
+#print(happy := True)
+
+foods = list()
+"""
+while True:
+    food = input("What food do you like?: ")
+    if food == "quit":
+        break
+    foods.append(food)
+"""
+
+while food := input("What food do you like?: ") != "quit":
+    foods.append(food)
+
