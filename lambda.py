3b1fd73601c0945fef7bfc468bd7b218909a265d
Zamishailo Artem <72965392+ZamArtem@users.noreply.github.com>
2023-01-05 23:49:12 +0100

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,17 @@
+
+
+"""
+def double(x):
+
+    return x * 2
+
+print(double(5))
+"""
+
+
+multiply    = lambda x, y: x * y
+double      = lambda x:x * 2
+add         = lambda x, y, z: x + y + z 
+full_name   = lambda first_name, last_name: first_name + " " + last_name
+age_check   = lambda age:True if age >= 18 else False
+#print(age_check(18))
