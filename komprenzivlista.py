87af31fc2f77caef2eabc8d2eb14dc532995c02a
Zamishailo Artem <72965392+ZamArtem@users.noreply.github.com>
2023-01-09 18:47:09 +0100

    Add files via upload

---
+++ komprenzivlista.py
@@ -0,0 +1,19 @@
+# list = [expression for item in iterable]
+#           komprenziv lista
+"""
+squares = []                    # create a list
+for i in range(1,11):           # create a for loop
+    squares.append(i * i)       # define what to do
+print(squares)
+"""
+#squares = [i * i for i in range(1,11)]
+
+students = [100,90,80,70,60,50,40,30,0]
+"""
+passed_students = list(filter(lambda x:x >= 60, students))
+print(passed_students)
+"""
+#   passed_students = [i for i in students if i >= 60]
+
+passed_students = [i if i >= 60 else "FAILED" for i in students]
+print(passed_students)
